47e0818f4d70104c55e94e77828f3e6dfd2f9674
wagnerquadros <woquadros@gmail.com>
2026-04-02 20:35:22 -0300

    chore: estrutura inicial do projeto

---
+++ src/main/java/com/wagnerquadros/simulapronaf/SimulapronafApplication.java
@@ -0,0 +1,13 @@
+package com.wagnerquadros.simulapronaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimulapronafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimulapronafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wagnerquadros/simulapronaf/SimulapronafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wagnerquadros.simulapronaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimulapronafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
