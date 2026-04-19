9072d1e3e1c4db31ef004a584dcd65d913df42c8
wagnerquadros <woquadros@gmail.com>
2026-04-19 17:29:45 -0300

    docs: atualiza README com instruções de Docker

--- src/main/java/com/wagnerquadros/simulapronaf/infraestrutura/configuracao/security/JwtConfig.java
+++ src/main/java/com/wagnerquadros/simulapronaf/infraestrutura/configuracao/security/JwtConfig.java
@@ -6,11 +6,8 @@ import com.nimbusds.jose.jwk.RSAKey;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.Resource;
 import org.springframework.security.oauth2.jwt.*;
-import org.springframework.util.FileCopyUtils;
 
-import java.nio.charset.StandardCharsets;
 import java.security.KeyFactory;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
